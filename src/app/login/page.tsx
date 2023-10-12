e627d864d9d6a42d5137f65dc4f0d5b5b3d2561b
Naman-9 <namancp7@gmail.com>
2023-10-12 11:21:34 +0530

    Folder structure-back&front, database Connection

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <div className="text-center justify-end items-center">
+        <h1>Login Page</h1>
+    </div>
+  )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignupPage() {
+    return (
+      <div className="text-center justify-end items-center">
+          <h1>Sign Page</h1>
+      </div>
+    )
+  }
+  
